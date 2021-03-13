9d7df3ae463f9f90659d324ab5f0c9bc9a983276
Alexandre Montesinos Cavallé <58102601+traxito@users.noreply.github.com>
2021-03-13 14:02:54 +0100

    Update provision.sh

--- provision/provision.sh
+++ provision/provision.sh
@@ -14,5 +14,6 @@ chmod 600 /root/.ssh/id_rsa
 cp /root/vagrant_ansible/provision/hosts /etc/
 cp /root/vagrant_ansible/provision/sudoers /etc/
 git clone https://github.com/traxito/vagrant_redis.git
-ip=$(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
-echo $ip "ansible_connection=local package=ftp" >> /etc/ansible/hosts
+#on hyper-v to automatically know the IP of the host.
+#ip=$(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+#echo $ip "ansible_connection=local package=ftp" >> /etc/ansible/hosts
